a302f7026d0c67b8a172396ed24004daa55f6d0b
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-07-28 00:40:27 +0200

    Removes unused functions from the stack.go module

--- internal/stack.go
+++ internal/stack.go
@@ -10,11 +10,6 @@ func MakeStack[TValue any](initialValues ...TValue) Stack[TValue] {
 	return s
 }
 
-func (s *Stack[TValue]) emptyValue() TValue {
-	var value TValue
-	return value
-}
-
 func (s *Stack[TValue]) Any() bool {
 	return s.IsEmpty() == false
 }
@@ -27,31 +22,9 @@ func (s *Stack[TValue]) Push(value ...TValue) {
 	*s = append(*s, value...)
 }
 
-func (s *Stack[TValue]) TryPop() (TValue, bool) {
-	if s.IsEmpty() {
-		return s.emptyValue(), false
-	} else {
-		return s.Pop(), true
-	}
-}
-
 func (s *Stack[TValue]) Pop() TValue {
 	lastItemIndex := len(*s) - 1
 	element := (*s)[lastItemIndex]
 	*s = (*s)[:lastItemIndex]
 	return element
-}
-
-func (s *Stack[TValue]) Peek() TValue {
-	lastItemIndex := len(*s) - 1
-	element := (*s)[lastItemIndex]
-	return element
-}
-
-func (s *Stack[TValue]) TryPeek() (TValue, bool) {
-	if s.IsEmpty() {
-		return s.emptyValue(), false
-	} else {
-		return s.Peek(), true
-	}
 }
