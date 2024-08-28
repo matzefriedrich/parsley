24395d4f01399c2b5587f1d21df3a4e1ef82cc00
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-08-28 21:16:49 +0200

    Updates the reference to the cobra-extensions package and sets the application name and description

--- cmd/parsley-cli/main.go
+++ cmd/parsley-cli/main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 
-	app := charmer.NewCommandLineApplication()
+	description := "Welcome to Parsley \U0001F33F CLI! \n\n" +
+		"Simplifying Advanced Dependency Injection in Go \n\n" +
+		"With Parsley, you can generate boilerplate code for advanced DI features effortlessly. " +
+		"Whether you're working with proxies, decorators, or need support for dynamic dependency resolution, " +
+		"Parsley CLI has you covered. Focus on your core business logic while it takes care of the heavy lifting."
+
+	app := charmer.NewCommandLineApplication("parsley-cli", description)
 
 	app.AddGroupCommand(
 		commands.NewGenerateGroupCommand(),
