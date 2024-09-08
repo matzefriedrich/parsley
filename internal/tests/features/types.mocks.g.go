8911da8e4cb07d398d95c50f08c3cc8c22a4f70f
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-08 13:52:46 +0200

    Completes the mocks.gotmpl text template

--- internal/tests/features/types.mocks.g.go
+++ internal/tests/features/types.mocks.g.go
@@ -8,17 +8,21 @@ type greeterMock struct {
 type SayHelloFunc func(name string) (string, error)
 type SayNothingFunc func()
 
-func (m greeterMock) SayHello(name string) (string, error) {
+func (m *greeterMock) SayHello(name string) (string, error) {
 	return m.sayHello(name)
 }
 
-func (m greeterMock) SayNothing() {
+func (m *greeterMock) SayNothing() {
 	m.sayNothing()
 }
 
 var _ Greeter = (*greeterMock)(nil)
 
-func NewGreeterMock(sayHello SayHelloFunc, sayNothing SayNothingFunc) *greeterMock {
+// NewGreeterMock Creates a new configurable greeterMock object.
+func NewGreeterMock(
+	sayHello SayHelloFunc,
+	sayNothing SayNothingFunc,
+) *greeterMock {
 	return &greeterMock{
 		sayHello:   sayHello,
 		sayNothing: sayNothing,
