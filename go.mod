01407036d689b47d9f56c62e9a97f2294d3f6c9d
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-09 16:01:36 +0200

    Initial implementation of the version command

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/matzefriedrich/parsley
 go 1.23
 
 require (
+	github.com/hashicorp/go-version v1.7.0
 	github.com/matzefriedrich/cobra-extensions v0.2.6
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.8.1
---
+++ internal/utils/version.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hashicorp/go-version"
+	"regexp"
+	"strconv"
+)
+
+var (
+	CommitSha     string
+	VersionString string = "0.1.0"
+)
+
+type VersionInfo struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+func (v VersionInfo) String() string {
+	return fmt.Sprintf("%d.%d.%d", v.Major, v.Minor, v.Patch)
+}
+
+func (v VersionInfo) LessThan(other VersionInfo) bool {
+	a, _ := version.NewVersion(v.String())
+	b, _ := version.NewVersion(other.String())
+	return a.LessThan(b)
+}
+
+func (v VersionInfo) Equal(other VersionInfo) bool {
+	a, _ := version.NewVersion(v.String())
+	b, _ := version.NewVersion(other.String())
+	return a.Equal(b)
+}
+
+func ApplicationVersion() (*VersionInfo, error) {
+	version, err := tryParseVersionInfo(VersionString)
+	if err != nil {
+		return nil, errors.New("application version not set")
+	}
+	return version, nil
+}
+
+func tryParseVersionInfo(version string) (*VersionInfo, error) {
+
+	re := regexp.MustCompile("(?:[vV])?(?P<major>\\d+)\\.(?P<minor>\\d+)\\.(?P<patch>\\d+)")
+	match := re.FindStringSubmatch(version)
+	if match == nil {
+		return nil, errors.New("invalid version")
+	}
+
+	extracted := map[string]string{}
+	names := re.SubexpNames()
+	for _, name := range names {
+		index := re.SubexpIndex(name)
+		if index != -1 && len(name) > 0 {
+			extracted[name] = match[index]
+		}
+	}
+
+	readInt := func(name string) int {
+		value, found := extracted[name]
+		if found {
+			n, err := strconv.Atoi(value)
+			if err == nil {
+				return n
+			}
+		}
+		return 0
+	}
+
+	major := readInt("major")
+	minor := readInt("minor")
+	patch := readInt("patch")
+
+	return &VersionInfo{Major: major, Minor: minor, Patch: patch}, nil
+}
