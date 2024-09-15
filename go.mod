3b993091709ba077585e09072880f2b9bc26b507
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-15 21:28:08 +0200

    Adds traces to the resolver.go and instances.go modules

--- go.mod
+++ go.mod
@@ -13,9 +13,14 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	go.opentelemetry.io/otel v1.30.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/core/instances.go
+++ internal/core/instances.go
@@ -38,43 +38,59 @@ func NewInstancesBag(parent *InstanceBag, scope types.LifetimeScope) *InstanceBa
 
 // TryResolveInstance attempts to locate an instance of a service identified by the given registration.
 func (b *InstanceBag) TryResolveInstance(ctx context.Context, registration types.ServiceRegistration) (interface{}, bool) {
+
+	ctx, span := newTryResolveInstanceSpan(scope)
+	defer span.End()
+
 	id := registration.Id()
 	instance, found := b.instances[id]
 	if found {
+		span.InstanceFound(id, SourceLocal)
 		return instance, true
 	}
 	scopedInstances, hasParsleyContext := ctx.Value(ParsleyContext).(map[uint64]interface{})
 	if hasParsleyContext {
 		instance, found = scopedInstances[id]
 		if found {
+			span.InstanceFound(id, SourceScope)
 			return instance, true
 		}
 	}
+
+	span.InstanceNotFound()
 	return nil, false
 }
 
 // KeepInstance stores an instance of a service based on the service's lifetime scope. Singleton instances are stored
 // at the appropriate singleton level in the hierarchy. Scoped instances are stored in the context-specified scope.
 // Transient instances are stored in the current instance bag.
-func (b *InstanceBag) KeepInstance(ctx context.Context, registration types.ServiceRegistration, instance interface{}) {
+func (b *InstanceBag) KeepInstance(scope context.Context, registration types.ServiceRegistration, instance interface{}) {
+
+	ctx, span := newKeepInstanceSpan(scope, registration)
+	defer span.End()
+
 	id := registration.Id()
 	switch registration.LifetimeScope() {
 	case types.LifetimeSingleton:
 		if b.scope == types.LifetimeSingleton {
+			span.InstanceStorage(StorageLocationLocalSingleton)
 			b.instances[id] = instance
 		} else {
 			if b.parent != nil {
+				span.InstanceStorage(StorageLocationParentSingleton)
 				b.parent.KeepInstance(ctx, registration, instance)
 			}
 		}
 	case types.LifetimeScoped:
 		scopedInstances, hasParsleyContext := ctx.Value(ParsleyContext).(map[uint64]interface{})
 		if hasParsleyContext {
+			span.InstanceStorage(StorageLocationScope)
 			scopedInstances[id] = instance
 		}
 	case types.LifetimeTransient:
 		fallthrough
 	default:
+		span.InstanceStorage(StorageLocationTransient)
 		b.instances[id] = instance
 	}
 }
