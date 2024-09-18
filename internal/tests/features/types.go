a2eda77e57e6f2a12a1502438251d72220951de0
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-19 00:52:50 +0200

    Removes unused interface

--- internal/tests/features/types.go
+++ internal/tests/features/types.go
@@ -1,7 +1,5 @@
 package features
 
-import "net/http"
-
 //go:generate parsley-cli generate proxy
 //go:generate parsley-cli generate mocks
 
@@ -9,9 +7,4 @@ import "net/http"
 type Greeter interface {
 	SayHello(name string, polite bool) (string, error)
 	SayNothing()
-}
-
-//parsley:mock
-type HttpClient interface {
-	Do(req *http.Request) (*http.Response, error)
 }
