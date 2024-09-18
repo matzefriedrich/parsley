f91c2510b65873419fb5f5cb56dafa7372cae401
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-18 18:35:39 +0200

    Adds httpClientMock to the github_api_client_test.go test module

--- internal/tests/utils/github_api_client_test.go
+++ internal/tests/utils/github_api_client_test.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"bytes"
 	"context"
 	"github.com/matzefriedrich/parsley/internal/utils"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http"
 	"testing"
 )
@@ -11,7 +13,19 @@ import (
 func Test_GitHubApiClient_QueryLatestReleaseTag(t *testing.T) {
 
 	// Arrange
-	client := &http.Client{}
+	const data = "[ { \"id\": 123, \"tag_name\": \"v0.1.0\", \"name\": \"release v0.1.0\" } ]"
+	buffer := bytes.Buffer{}
+	buffer.WriteString(data)
+
+	body := &httpResponseMock{responseBuffer: buffer}
+	client := &httpClientMock{
+		DoFunc: func(req *http.Request) (*http.Response, error) {
+			return &http.Response{
+				Body:       body,
+				StatusCode: http.StatusOK,
+			}, nil
+		},
+	}
 	sut := utils.NewGitHubApiClient(client, func(options *utils.HttpClientOptions) {
 
 	})
@@ -23,3 +37,27 @@ func Test_GitHubApiClient_QueryLatestReleaseTag(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotEmpty(t, tag)
 }
+
+type httpClientMock struct {
+	DoFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (h *httpClientMock) Do(req *http.Request) (*http.Response, error) {
+	return h.DoFunc(req)
+}
+
+var _ utils.HttpClient = (*httpClientMock)(nil)
+
+type httpResponseMock struct {
+	responseBuffer bytes.Buffer
+}
+
+func (h *httpResponseMock) Read(p []byte) (n int, err error) {
+	return h.responseBuffer.Read(p)
+}
+
+func (h *httpResponseMock) Close() error {
+	return nil
+}
+
+var _ io.ReadCloser = (*httpResponseMock)(nil)
