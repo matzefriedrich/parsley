f47bca452e25a3ce2dc296dc874bad383143153e
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-19 22:47:38 +0200

    Adds documentation comments to the pkg/types package

--- pkg/types/parsley_error.go
+++ pkg/types/parsley_error.go
@@ -2,25 +2,31 @@ package types
 
 import "errors"
 
+// ParsleyError represents an error with an associated message and optional underlying cause.
 type ParsleyError struct {
 	cause error
 	Msg   string
 }
 
+// Error returns the message associated with the ParsleyError.
 func (f ParsleyError) Error() string {
 	return f.Msg
 }
 
+// Unwrap returns the underlying cause of the ParsleyError, allowing for error unwrapping functionality.
 func (f ParsleyError) Unwrap() error {
 	return f.cause
 }
 
+// Is compares the current ParsleyError's message with another error's message to determine if they are the same.
 func (f ParsleyError) Is(err error) bool {
 	return f.Error() == err.Error()
 }
 
+// ParsleyErrorFunc is a function type that modifies a given error.
 type ParsleyErrorFunc func(e error)
 
+// WithCause wraps a given error within a ParsleyError.
 func WithCause(err error) ParsleyErrorFunc {
 	return func(e error) {
 		var funqErr *ParsleyError
@@ -31,10 +37,12 @@ func WithCause(err error) ParsleyErrorFunc {
 	}
 }
 
+// ParsleyErrorWithServiceTypeName defines an interface for setting the service type name on errors.
 type ParsleyErrorWithServiceTypeName interface {
 	ServiceTypeName(name string)
 }
 
+// ForServiceType creates a ParsleyErrorFunc that sets the service type name on errors that implement the ParsleyErrorWithServiceTypeName interface.
 func ForServiceType(serviceType string) ParsleyErrorFunc {
 	return func(e error) {
 		withServiceTypeErr, ok := e.(ParsleyErrorWithServiceTypeName)
@@ -44,15 +52,18 @@ func ForServiceType(serviceType string) ParsleyErrorFunc {
 	}
 }
 
+// ParsleyAggregateError represents an aggregate of multiple errors.
 type ParsleyAggregateError struct {
 	errors []error
 	Msg    string
 }
 
+// Error returns the message associated with the ParsleyAggregateError.
 func (f ParsleyAggregateError) Error() string {
 	return f.Msg
 }
 
+// Is checks if the given error is equivalent to any error within the ParsleyAggregateError.
 func (f ParsleyAggregateError) Is(err error) bool {
 	if f.Error() == err.Error() {
 		return true
@@ -65,6 +76,7 @@ func (f ParsleyAggregateError) Is(err error) bool {
 	return false
 }
 
+// WithAggregatedCause returns a ParsleyErrorFunc that sets an aggregated error cause with the provided errors.
 func WithAggregatedCause(errs ...error) ParsleyErrorFunc {
 	return func(e error) {
 		var funqErr *ParsleyError
--- pkg/types/service_type.go
+++ pkg/types/service_type.go
@@ -13,33 +13,41 @@ type serviceType struct {
 	lookupKey     ServiceKey
 }
 
+// String returns a formatted string representation of the service type, including its name, and package path.
 func (s serviceType) String() string {
 	return fmt.Sprintf("Name: \"%s\", Package: \"%s\", List: %t)", s.name, s.packagePath, s.list)
 }
 
 var _ ServiceType = &serviceType{}
 
+// LookupKey returns the lookup key associated with the service type.
 func (s serviceType) LookupKey() ServiceKey {
 	return s.lookupKey
 }
 
+// ReflectedType returns the reflected type of the service.
 func (s serviceType) ReflectedType() reflect.Type {
 	return s.reflectedType
 }
 
+// Name returns the name of the service type.
 func (s serviceType) Name() string {
 	return s.name
 }
 
+// PackagePath returns the package path of the service type.
 func (s serviceType) PackagePath() string {
 	return s.packagePath
 }
 
+// MakeServiceType creates a ServiceType instance for the specified generic type T.
 func MakeServiceType[T any]() ServiceType {
 	elem := reflect.TypeOf(new(T)).Elem()
 	return ServiceTypeFrom(elem)
 }
 
+// ServiceTypeFrom creates a ServiceType from the given reflect.Type.
+// Supports pointer, interface, function, slice, and struct types. The function panics, if t is of an unsupported kind is given.
 func ServiceTypeFrom(t reflect.Type) ServiceType {
 	isList := false
 	elemType := t
