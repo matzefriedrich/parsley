6f51d94ab70aabf168b64632ba809d116e5e15f4
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-21 00:32:47 +0200

    Adds functionality to find circular dependencies in service registrations

--- internal/tests/registration/validator_test.go
+++ internal/tests/registration/validator_test.go
@@ -1,6 +1,8 @@
 package registration
 
 import (
+	"errors"
+	"fmt"
 	"github.com/matzefriedrich/parsley/internal/tests/features"
 	"github.com/matzefriedrich/parsley/pkg/registration"
 	"github.com/matzefriedrich/parsley/pkg/types"
@@ -49,6 +51,36 @@ func Test_Validator_Validate_missing_dependency_does_return_error(t *testing.T) 
 
 	// Assert
 	assert.ErrorIs(t, err, registration.ErrRegistryMissesRequiredServiceRegistrations)
+
+	var aggregateErr *types.ParsleyAggregateError
+	if errors.As(err, &aggregateErr) {
+		for _, e := range aggregateErr.Errors() {
+			fmt.Println(e.Error())
+		}
+	}
+}
+
+func Test_Validator_Validate_detects_circular_dependencies_and_returns_error(t *testing.T) {
+
+	// Arrange
+	registry := registration.NewServiceRegistry()
+	_ = registry.Register(newTestService2, types.LifetimeTransient)
+	_ = registry.Register(newTestService3, types.LifetimeTransient)
+
+	sut := registration.NewServiceRegistrationsValidator()
+
+	// Act
+	err := sut.Validate(registry)
+
+	// Assert
+	assert.ErrorIs(t, err, registration.ErrCircularDependencyDetected)
+
+	var aggregateErr *types.ParsleyAggregateError
+	if errors.As(err, &aggregateErr) {
+		for _, e := range aggregateErr.Errors() {
+			fmt.Println(e.Error())
+		}
+	}
 }
 
 type testService struct {
@@ -58,5 +90,25 @@ type testService struct {
 func newTestService(greeter features.Greeter) *testService {
 	return &testService{
 		greeter: greeter,
+	}
+}
+
+type testService2 struct {
+	other *testService3
+}
+
+func newTestService2(other *testService3) *testService2 {
+	return &testService2{
+		other: other,
+	}
+}
+
+type testService3 struct {
+	other *testService2
+}
+
+func newTestService3(other *testService2) *testService3 {
+	return &testService3{
+		other: other,
 	}
 }
--- pkg/registration/validator.go
+++ pkg/registration/validator.go
@@ -10,20 +10,32 @@ import (
 const (
 	ErrorFailedToRetrieveServiceRegistrations       = "failed to retrieve service registrations"
 	ErrorRegistryMissesRequiredServiceRegistrations = "the registry misses required service registrations"
+	ErrorCircularDependencyDetected                 = "circular dependencies detected"
 )
 
 var (
-	ErrFailedToRetrieveServiceRegistrations       = types.NewRegistryError(ErrorFailedToRetrieveServiceRegistrations)
+
+	// ErrFailedToRetrieveServiceRegistrations signifies an error encountered while attempting to retrieve service registrations.
+	ErrFailedToRetrieveServiceRegistrations = types.NewRegistryError(ErrorFailedToRetrieveServiceRegistrations)
+
+	// ErrRegistryMissesRequiredServiceRegistrations indicates that required service registrations are missing.
 	ErrRegistryMissesRequiredServiceRegistrations = types.NewRegistryError(ErrorRegistryMissesRequiredServiceRegistrations)
+
+	// ErrCircularDependencyDetected signifies that a circular dependency was encountered.
+	ErrCircularDependencyDetected = types.NewResolverError(types.ErrorCircularDependencyDetected)
 )
 
+// Validator defines an interface to validate service registries..
 type Validator interface {
+
+	// Validate checks the provided ServiceRegistry for missing, invalid, or circular service dependencies. Returns an error if any issues are found.
 	Validate(registry types.ServiceRegistry) error
 }
 
 type serviceRegistrationsValidator struct {
 }
 
+// Validate ensures that all required service registrations are present and service do not depend on them-selves (prevents circular dependencies).
 func (s *serviceRegistrationsValidator) Validate(registry types.ServiceRegistry) error {
 
 	registrations, err := registry.GetServiceRegistrations()
@@ -40,7 +52,7 @@ func (s *serviceRegistrationsValidator) Validate(registry types.ServiceRegistry)
 		stack.Push(registration)
 	}
 
-	for stack.IsEmpty() == false {
+	for stack.Any() {
 		next := stack.Pop()
 		nextId := next.Id()
 		_, seen := checkedServiceTypes[nextId]
@@ -68,11 +80,58 @@ func (s *serviceRegistrationsValidator) Validate(registry types.ServiceRegistry)
 		return types.NewRegistryError(ErrorRegistryMissesRequiredServiceRegistrations, types.WithAggregatedCause(errors...))
 	}
 
+	circularDependencyErrors := make([]error, 0)
+	for _, registration := range registrations {
+		if dependencyError := detectCircularDependency(registration, registry); dependencyError != nil {
+			serviceType := registration.ServiceType()
+			circularDependencyError := types.NewRegistryError(ErrorCircularDependencyDetected,
+				types.WithCause(dependencyError),
+				types.ForServiceType(serviceType.Name()))
+			circularDependencyErrors = append(circularDependencyErrors, circularDependencyError)
+		}
+	}
+
+	if len(circularDependencyErrors) > 0 {
+		return types.NewRegistryError(ErrorCircularDependencyDetected, types.WithAggregatedCause(circularDependencyErrors...))
+	}
+
+	return nil
+}
+
+func detectCircularDependency(sr types.ServiceRegistration, registry types.ServiceRegistry) error {
+
+	stack := internal.MakeStack[types.ServiceRegistration]()
+
+	pushRequiredServices := func(r types.ServiceRegistration) {
+		requiredServices := r.RequiredServiceTypes()
+		for _, serviceType := range requiredServices {
+			list, found := registry.TryGetServiceRegistrations(serviceType)
+			if found == false {
+				continue
+			}
+			for _, item := range list.Registrations() {
+				stack.Push(item)
+			}
+		}
+	}
+
+	pushRequiredServices(sr)
+
+	for stack.Any() {
+		next := stack.Pop()
+		if next.Id() == sr.Id() {
+			serviceType := sr.ServiceType()
+			return fmt.Errorf("circular dependency detected for service type %s", serviceType.Name())
+		}
+		pushRequiredServices(next)
+	}
+
 	return nil
 }
 
 var _ Validator = (*serviceRegistrationsValidator)(nil)
 
+// NewServiceRegistrationsValidator creates a new Validator instance.
 func NewServiceRegistrationsValidator() Validator {
 	return &serviceRegistrationsValidator{}
 }
