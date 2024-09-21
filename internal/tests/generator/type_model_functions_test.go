bb4127d775fd59e864377c4253e745771d0e7cf7
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-22 01:05:59 +0200

    Adds new tests for the type_model_functions.go module

--- internal/tests/generator/type_model_functions_test.go
+++ internal/tests/generator/type_model_functions_test.go
@@ -105,6 +105,15 @@ func Test_FormatType_array_pointer_type(t *testing.T) {
 	assert.Equal(t, "[]*Arg", actual)
 }
 
+func Test_FormatType_returns_any_if_parameter_type_is_nil(t *testing.T) {
+	// Arrange
+	p := reflection.Parameter{Name: "p", Type: nil}
+	// Act
+	actual := generator.FormatType(p)
+	// Assert
+	assert.Equal(t, "any", actual)
+}
+
 func Test_FormattedCallParameters_single_parameter(t *testing.T) {
 	// Arrange
 	m := reflection.Method{
@@ -120,6 +129,28 @@ func Test_FormattedCallParameters_single_parameter(t *testing.T) {
 	actual := generator.FormattedCallParameters(m)
 	// Assert
 	assert.Equal(t, "p", actual)
+}
+
+func Test_FormattedCallParameters_format_ellipsis(t *testing.T) {
+	// Arrange
+	m := reflection.Method{
+		Name: "SayHello",
+		Parameters: []reflection.Parameter{
+			{
+				Name: "p",
+				Type: &reflection.ParameterType{
+					IsEllipsis: true,
+					Next: &reflection.ParameterType{
+						Name: "string"},
+				},
+			},
+		},
+	}
+
+	// Act
+	actual := generator.FormattedCallParameters(m)
+	// Assert
+	assert.Equal(t, "p...", actual)
 }
 
 func Test_FormattedCallParameters_multiple_parameter(t *testing.T) {
@@ -186,6 +217,18 @@ func Test_FormattedParameters_multiple_parameter(t *testing.T) {
 	assert.Equal(t, "s string, buffer []byte, b bool", actual)
 }
 
+func Test_FormattedResultParameters_returns_empty_string_if_Results_is_nil(t *testing.T) {
+
+	// Arrange
+	m := reflection.Method{Name: "SayHello", Results: nil}
+
+	// Act
+	actual := generator.FormattedResultParameters(m)
+
+	// Assert
+	assert.Equal(t, "", actual)
+}
+
 func Test_FormattedResultParameters_single_parameter(t *testing.T) {
 	// Arrange
 	m := reflection.Method{
@@ -203,7 +246,7 @@ func Test_FormattedResultParameters_single_parameter(t *testing.T) {
 	assert.Equal(t, "result0", actual)
 }
 
-func Test_FormattedResultParameters_multiple_parameter(t *testing.T) {
+func Test_FormattedResultParameters_multiple_parameters(t *testing.T) {
 	// Arrange
 	m := reflection.Method{
 		Name: "SayHello",
@@ -219,4 +262,46 @@ func Test_FormattedResultParameters_multiple_parameter(t *testing.T) {
 	actual := generator.FormattedResultParameters(m)
 	// Assert
 	assert.Equal(t, "result0, result1", actual)
+}
+
+func Test_FormattedResultTypes_returns_empty_string_if_Results_is_nil(t *testing.T) {
+	// Arrange
+	m := reflection.Method{Name: "SayHello", Results: nil}
+	// Act
+	actual := generator.FormattedResultTypes(m)
+	// Assert
+	assert.Equal(t, "", actual)
+}
+
+func Test_FormattedResultTypes_multiple_parameters(t *testing.T) {
+	// Arrange
+	m := reflection.Method{
+		Name: "SayHello",
+		Results: []reflection.Parameter{
+			{Name: "result0", Type: &reflection.ParameterType{Name: "string"}},
+			{Name: "result1", Type: &reflection.ParameterType{Name: "error"}},
+		},
+	}
+	// Act
+	actual := generator.FormattedResultTypes(m)
+	// Assert
+	assert.Equal(t, "(string, error)", actual)
+}
+
+func Test_Signature_formats_method_name_parameters_and_result_types(t *testing.T) {
+	// Arrange
+	m := reflection.Method{
+		Name: "SayHello",
+		Parameters: []reflection.Parameter{
+			{Name: "s", Type: &reflection.ParameterType{Name: "string"}},
+		},
+		Results: []reflection.Parameter{
+			{Name: "result0", Type: &reflection.ParameterType{Name: "string"}},
+			{Name: "result1", Type: &reflection.ParameterType{Name: "error"}},
+		},
+	}
+	// Act
+	actual := generator.Signature(m)
+	// Assert
+	assert.Equal(t, "SayHello(s string) (string, error)", actual)
 }
